b17df8cf2b77b6871fa8564ca4f0b996aaa344b5
di858mon <di858mon@gmail.com>
2021-10-18 09:48:18 +0400

    add store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { createStore } from 'redux';
 import App from "./App";
+import rootReducer from "./reducers";
+import initialState from "./store/initialState";
+
+const store = createStore (rootReducer, initialState);
 
 ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/store/initialState.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const initialState = {
+  users: [
+    {
+      name: 'Петя',
+      password: '******',
+      age: 69,
+    },
+  ],
+};
+
+export default initialState;
