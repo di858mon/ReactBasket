91d443c272769fe48dd757c0fcb1219b0e392592
di858mon <di858mon@gmail.com>
2021-10-19 09:05:41 +0400

    finish store

--- src/store/index.js
+++ src/store/index.js
@@ -1,2 +1,7 @@
-import {configureStore} from "@reduxjs/toolkit"
+import React from "react";
+import { configureStore } from '@reduxjs/toolkit';
+import goodsReducer from "./goodsSlice";
 
+export default configureStore({
+  reducer: {},
+});
